f4efc7745903f4270c55cc0ca09d3871f3e78775
Utshah Neupane <utshah777@gmail.com>
2025-07-22 14:46:27 -0500

    good tillnow

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'a921454d727093c2b06b82c7'
 db = SQLAlchemy(app)
 
 
--- seed_db.py
+++ seed_db.py
@@ -3,7 +3,7 @@ from market.models import Item, User
 
 with app.app_context():
     
-    if not User.query.filter_by(username = "John").first:
+    if not User.query.filter_by(username = "John").first():
         u1 = User(username = "John", password_hash = '123456', email_address = 'john@gmail.com')
         db.session.add(u1)
 
@@ -19,20 +19,39 @@ with app.app_context():
     if not Item.query.filter_by(name="Tablet").first():
         item3 = Item(name="Tablet", price=1100, barcode="579305222278", description="Tablet is good!")
         db.session.add(item3)
-         
-
+    
+    db.session.commit()   # Commit here so user exists before setting as owner
+    
+    
+    item1.owner = User.query.filter_by(username = "John").first().id
+    db.session.add(item1)
     db.session.commit()
     
     
 
     for item in Item.query.all():
         print(f"{item.name}: {item.price}, {item.barcode}, {item.description}")
+        
+    
+    print(item1.owner)  #Gives id of the owner
+    
+    i = Item.query.filter_by(name = "IPhone10").first()
+    print(i.owned_user)
+    
+    
+    print(User.query.all())
+    print(Item.query.all())  #gives as a list
+    
+    
+    
 
     # for item in Item.query.filter_by(price=500):
     #     print(item.name)
         
     
     # print(Item.query.all())  #gives as a list
+    
+    # db.session.rollback()   #makes previous changes not applicable 
 
 
 
